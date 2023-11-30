f43755094c3e79c333c0479668d29aee9e6c8b79
hongsheng <1634340724@qq.com>
2023-12-01 02:02:22 +0800

    数组操作

--- day2/array.go
+++ day2/array.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+func change(a *[4]int) {
+	a[0] = 1
+}
+
+type node struct {
+	a int
+	b int
+}
+
+func main() {
+	var a [4]int
+	change(&a)
+	var i, v int
+	for i, v = range a {
+		fmt.Println(i, " ", v)
+	}
+	var b = [...]int{1, 2, 3}
+	fmt.Println(b[0])
+	fmt.Println()
+	var c = [...]node{
+		{1, 2},
+		{3, 4},
+	}
+	for s := range c {
+		fmt.Println(c[s])
+	}
+	fmt.Println()
+	var arr0 = [1][3]int{
+		{1, 2, 3},
+	}
+	fmt.Println(arr0)
 }
