466755987aa6559bb803d5590fde43e837ad325c
hongsheng <1634340724@qq.com>
2023-11-30 12:48:13 +0800

    字符串操作

--- day2/test.go
+++ day2/test.go
@@ -3,8 +3,27 @@ package main
 import (
 	"fmt"
 	_ "go-learning/day1"
+	"strings"
 )
+
+const b = 1
 
 func main() {
 	fmt.Println(1)
+	var a = true
+	fmt.Println(a)
+	fmt.Println(b, a)
+	var s = "ac34567"
+	s2 := s[1:len(s)]
+	fmt.Println(s2)
+	w := 1
+	fmt.Println(w)
+	var c = s[0]
+	fmt.Println(c)
+	bytes := []byte(s)
+	bytes[0] = 'c'
+	s = string(bytes)
+	fmt.Println(s)
+	replace := strings.Replace(s, string(s[0]), "黄", 3)
+	fmt.Println(replace)
 }
